c8654bf20f1acc48bd7e9983192fc98132d077c6
Zizod <mquadri70@gmail.com>
2022-12-11 07:03:11 +0530

    bug fixed

--- Jenkinsscript(ubuntu).sh
+++ Jenkinsscript(ubuntu).sh
@@ -8,5 +8,5 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
   sudo apt-get update
-  sudo apt-get install fontconfig openjdk-11-jre
-  sudo apt-get install jenkins
+  sudo apt-get install fontconfig openjdk-11-jre -y
+  sudo apt-get install jenkins -y
